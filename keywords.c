d84c065ed201e486d7e2b92e401f0d22c710bd7b
Lucas Silva <lucas.correia010103@gmail.com>
2024-12-02 19:07:07 -0300

    feat: criacao do controle de palavras chaves

---
+++ keywords.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <keywords.h>
+
+char *keyword[] = {
+    "begin",
+    "program",
+    "var",    
+    "integer",
+    "real",
+    "double",
+    "boolean",
+    "procedure",
+    "function",
+    "if",
+    "then",
+    "else",
+    "while",
+    "do",
+    "repeat",
+    "until",
+    "div",
+    "and",
+    "mod",
+    "or",
+    "not",
+    "end"
+};
+
+int iskeyword(char *word)
+{
+    for (int i = BEGIN; i <= END; i++){
+        if (strcmp(word, keyword[i - BEGIN]) == 0){
+            return i;
+        } 
+    }
+
+    return 0;
+}
---
+++ keywords.h
@@ -0,0 +1,31 @@
+#pragma once
+
+typedef enum _keyword_
+{
+    BEGIN = 0X10000, //65536
+    PROGRAM,
+    VAR,
+    INTEGER,
+    REAL,
+    DOUBLE,
+    BOOLEAN,
+    PROCEDURE,
+    FUNCTION,
+    IF,
+    THEN, 
+    ELSE,
+    WHILE,
+    DO,
+    REPEAT,
+    UNTIL,
+    DIV,
+    AND,
+    MOD,
+    OR,
+    NOT,
+    END
+} EKeyWord;
+
+extern char *keyword[];
+
+extern int iskeyword(char *word);
